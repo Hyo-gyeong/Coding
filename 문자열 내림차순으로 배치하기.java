2a5ed4c1f120998b9ae8819aa4815fce6a417187
Hyo-gyeong <50789692+Hyo-gyeong@users.noreply.github.com>
2021-02-04 21:50:04 +0900

    !미완

--- 문자열 내림차순으로 배치하기.java
+++ 문자열 내림차순으로 배치하기.java
@@ -4,7 +4,7 @@ class Solution {
         String[] tmp = new String[s.length()];
         for (int i = 0; i < s.length(); i++)
             tmp[i] = Character.toString(s.charAt(i));
-        Arrays.sort(tmp, Collections.reverseOrder());
+        Arrays.sort(tmp, Collections.reverseOrder()); //array내림차순 정렬
         String answer = "";
         for (int i = 0; i < s.length(); i++)
             answer += tmp[i];
@@ -13,17 +13,27 @@ class Solution {
     }
 }
 
+------------------------------------------------------------------------
+memo)
+sort()내부에 정렬할 배열과 내림차순 여부 쓸 수 있음.
+참고 https://www.geeksforgeeks.org/arrays-sort-in-java-with-examples/
+------------------------------------------------------------------------
+
 다른사람 풀이 예1)
 import java.util.*;
 class Solution {
     public String solution(String s) {
-        char[] ch = s.toCharArray();
+        char[] ch = s.toCharArray();//1.
         Arrays.sort(ch);
         StringBuffer st = new StringBuffer(String.valueOf(ch));
         st.reverse();
         return st.toString();
     }
 }
+------------------------------------------------------------------------
+memo)
+1.String into array of chars
+------------------------------------------------------------------------
 
 다른사람 풀이 예2)
 import java.util.*;
