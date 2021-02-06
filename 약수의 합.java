bd6016dfd8fc7364bd309c8310a5f970b6af0e62
Hyo-gyeong <50789692+Hyo-gyeong@users.noreply.github.com>
2021-02-06 16:38:09 +0900

    Math.sqrt

--- 약수의 합.java
+++ 약수의 합.java
@@ -4,7 +4,7 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         List<Integer> divisor = new ArrayList<Integer>();
-        for (int i = 1; i <= Math.sqrt(n); i++){
+        for (int i = 1; i <= Math.sqrt(n); i++){ //제곱근 반환. !약수의 중앙값은 제곱근을 넘지 않음.
             if (divisor.contains(i) == false && n%i == 0){
                 answer += i;
                 divisor.add(i);
