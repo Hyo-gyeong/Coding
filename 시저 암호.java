c328562e91c10f580f29ebab51684235931fb59d
Hyo-gyeong <50789692+Hyo-gyeong@users.noreply.github.com>
2021-02-06 14:39:04 +0900

    ascii int to char, char to int

--- 시저 암호.java
+++ 시저 암호.java
@@ -2,18 +2,22 @@ class Solution {
     public String solution(String s, int n) {
         String answer = "";
         for (int i = 0; i < s.length(); i++){
-            int to_int = (int)s.charAt(i);
-            if(to_int == ' ') {
-                answer += (char)to_int;
+            if(s.charAt(i) == ' ') { //공백처리
+                answer += s.charAt(i);
                 continue;
             }
+            int to_int = (int)s.charAt(i); //1.ascii
             int new_int = to_int+n;
             if (to_int <= (int)'z' && new_int > (int)'z' ||
                to_int <= (int)'Z' && new_int > (int)'Z')
                 new_int -= 26;
             answer += (char)new_int;
         }
-        System.out.println(answer);
         return answer;
     }
 }
+
+--------------------------------------------------------------------
+memo)
+1.(int)char형 = 문자를 ascii값으로, (char)int형 = ascii값을 해당 문자로
+--------------------------------------------------------------------
