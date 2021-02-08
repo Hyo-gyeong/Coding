5aa1c6e766b67e5ca4dca3a42db1ed092970fec1
Hyo-gyeong <50789692+Hyo-gyeong@users.noreply.github.com>
2021-02-09 02:12:57 +0900

    int <-> String

--- 자릿수 더하기.java
+++ 자릿수 더하기.java
@@ -3,9 +3,29 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        String str_n = Integer.toString(n);
+        String str_n = Integer.toString(n);//0.int to String
         for (int i = 0; i < str_n.length(); i++)
-            answer += Integer.parseInt(String.valueOf(str_n.charAt(i)));
+            answer += Integer.parseInt(String.valueOf(str_n.charAt(i))); //1.String to int
         return answer;
     }
 }
+
+---------------------------------------------------------------------------
+다른사람 풀이 보고 다시
+
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while(n > 0){
+            answer += n%10;
+            n /= 10;
+        }
+        return answer;
+    }
+}
+
+
+->뒤에서부터 더해도 똑같다는 생각!
+->형변환 필요 없음!
